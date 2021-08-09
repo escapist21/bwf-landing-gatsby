74d66807a04ed04a4f5e8942e714fb60829522b8
Sourav Adhikari <ad.sourav21@live.com>
2021-08-09 16:07:58 +0530

    made our services mobile responsive

--- src/components/our-services.js
+++ src/components/our-services.js
@@ -3,33 +3,63 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const OurServices = () => {
     return(
-        <div className="container my-5 py-5">
-            <div className="column">
-                <p className="title has-text-centered pb-5 has-text-weight-light is-size-2">
-                    OUR SERVICES
-                </p>
+        <div className="section">
+            <div id="desktop" className="container is-hidden-mobile">
+                <div className="column">
+                    <p className="title has-text-centered pb-4 has-text-weight-light is-size-2">
+                        OUR SERVICES
+                    </p>
+                </div>
+                <nav className="level is-mobile">
+                    <div className="level-item has-text-centered">
+                        <figure className="image is-128x128">
+                            <StaticImage src="../assets/BWF_icons_project_management.png" alt="" />
+                            <p className="title is-size-5 is-uppercase has-text-weight-light mb-5">Advisory & Project Management</p>
+                        </figure>
+                    </div>
+                    <div className="level-item has-text-centered">
+                        <figure className="image is-128x128">
+                            <StaticImage src="../assets/BWF_icons_research.png" alt="" />
+                            <p className="title is-size-5 is-uppercase has-text-weight-light">Research & Analytics</p>
+                        </figure>
+                    </div>
+                    <div className="level-item has-text-centered">
+                        <figure className="image is-128x128">
+                            <StaticImage src="../assets/BWF_icons_solutions.png" alt="" />
+                            <p className="title is-size-5 is-uppercase has-text-weight-light">Evidence-based Solutions</p>
+                        </figure>
+                    </div>
+                </nav>
             </div>
-            <nav className="level is-mobile">
-                <div className="level-item has-text-centered">
-                    <figure className="image is-128x128">
-                        <StaticImage src="../assets/BWF_icons_project_management.png" alt="" />
-                        <p className="title is-size-5 is-uppercase has-text-weight-light mb-5">Advisory & Project Management</p>
-                    </figure>
+            <div id="touch" className="container is-hidden-tablet">
+                <div className="column">
+                    <p className="title has-text-centered pb-4 has-text-weight-light is-size-2">
+                        OUR SERVICES
+                    </p>
                 </div>
-                <div className="level-item has-text-centered">
-                    <figure className="image is-128x128">
-                        <StaticImage src="../assets/BWF_icons_research.png" alt="" />
-                        <p className="title is-size-5 is-uppercase has-text-weight-light">Research & Analytics</p>
-                    </figure>
-                </div>
-                <div className="level-item has-text-centered">
-                    <figure className="image is-128x128">
-                        <StaticImage src="../assets/BWF_icons_solutions.png" alt="" />
-                        <p className="title is-size-5 is-uppercase has-text-weight-light">Evidence-based Solutions</p>
-                    </figure>
-                </div>
-            </nav>
+                <nav className="level is-mobile">
+                    <div className="level-item has-text-centered">
+                        <figure className="image is-96x96">
+                            <StaticImage src="../assets/BWF_icons_project_management.png" alt="" />
+                            <p className="title is-size-6 is-uppercase has-text-weight-light mb-5">Advisory & Project Management</p>
+                        </figure>
+                    </div>
+                    <div className="level-item has-text-centered">
+                        <figure className="image is-96x96">
+                            <StaticImage src="../assets/BWF_icons_research.png" alt="" />
+                            <p className="title is-size-6 is-uppercase has-text-weight-light">Research & Analytics</p>
+                        </figure>
+                    </div>
+                    <div className="level-item has-text-centered">
+                        <figure className="image is-96x96">
+                            <StaticImage src="../assets/BWF_icons_solutions.png" alt="" />
+                            <p className="title is-size-6 is-uppercase has-text-weight-light">Evidence-based Solutions</p>
+                        </figure>
+                    </div>
+                </nav>
+            </div>
         </div>
+        
     )
 }
 
