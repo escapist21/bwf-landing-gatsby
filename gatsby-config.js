da34123814b98d27c93fc7b229175ce348ba12fd
Sourav Adhikari <ad.sourav21@live.com>
2021-08-10 16:37:01 +0530

    added Gatsby helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     title: "bwf-gatsby",
   },
   plugins: [
+    "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
