4b8e2068a67792b4677a4ad19877f927c7c22e52
Sourav Adhikari <ad.sourav21@live.com>
2021-08-15 21:04:09 +0530

    added contact page

---
+++ src/pages/contact-us.js
@@ -0,0 +1,61 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Layout from "../components/layout"
+import ContactBanner from "../components/contact-banner"
+import ContactForm from "../components/contact-form"
+import { faCoffee, faEnvelope, faLocationArrow, faMapMarkedAlt, faMapMarker, faMapMarkerAlt, faPhone, faPhoneAlt } from "@fortawesome/free-solid-svg-icons"
+import { faAddressCard } from "@fortawesome/free-regular-svg-icons"
+
+
+const ContactPage = () => {
+    return(
+        <Layout>
+            <ContactBanner />
+            <section className="section">
+                <div className="container">
+                    <div className="columns">
+                        <div className="column is-5">
+                            <div className="section pt-0 pb-3">
+                                <p className="title has-text-weight-light has-text-link is-size-3 is-link">
+                                    Send us a message and we will surely reply
+                                </p>
+                            </div>
+                            <div className="section pt-0 pb-0">
+                                <ContactForm />
+                            </div>
+                        </div>
+                        <div className="column is-2">
+                        </div>
+                        <div className="column is-5">
+                            <div className="section pt-0 pb-3">
+                                <p className="title has-text-weight-light has-text-link is-size-3 is-link">
+                                    Reach out to us
+                                </p>
+                            </div>
+                            <div className="section pt-0 pb-3">
+                                <table className="table is-borderless">
+                                    <tbody>
+                                        <tr>
+                                            <td><FontAwesomeIcon icon={ faMapMarkerAlt} size="lg" className="font-awesome-color"/></td>
+                                            <td>Better World Foundation, <br />Pankipani, Ward-55, <br />Kumhrar House No-464, Maurya Vihar, <br/> Near Transport Nagar, <br />Patna, 800026</td>
+                                        </tr>
+                                        <tr>
+                                            <td><FontAwesomeIcon icon={ faEnvelope} size="lg" className="font-awesome-color"/></td>
+                                            <td><a href="mailto:connect@bwf.org.in">connect@bwf.org.in</a></td>
+                                        </tr>
+                                        <tr>
+                                            <td><FontAwesomeIcon icon={ faPhoneAlt} size="lg" className="font-awesome-color"/></td>
+                                            <td>+91 9999-999-999</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Layout>
+    )
+}
+
+export default ContactPage
