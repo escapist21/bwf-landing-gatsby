beb6c1f6bb428e5e46820b5a633b538798aad0e0
Sourav Adhikari <ad.sourav21@live.com>
2021-08-16 11:24:16 +0530

    added airform mail servers

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,35 +1,36 @@
-import { div } from "prelude-ls"
 import React from "react"
+import Airform from "react-airform"
 
+// const formsubmit = "ef4a883d4e661aecf08301bf355eddba"
 const ContactForm = () => {
     return(
         <div className="container">
-            <form target="_blank" action="https://formsubmit.co/connect@bwf.org.in" method="POST">
+            <form target="_blank" action="https://airform.io/connect@bwf.org.in" method="POST">
                 <div className="field">
                     <label className="label">Name</label>
                     <div className="control">
-                        <input className="input" type="text" placeholder="Enter name"></input>
+                        <input className="input" name="name" type="text" placeholder="Enter name"></input>
                     </div>
                 </div>
                 <div className="field">
                     <label className="label">Email</label>
                     <div className="control">
-                        <input className="input" type="email" placeholder="Enter email" />
+                        <input className="input" name="email" type="email" placeholder="Enter email" />
                     </div>
                 </div>
                 <div className="field">
                     <label className="label">Phone number</label>
                     <div className="control">
-                        <input className="input" type="text" placeholder="Enter phone number"></input>
+                        <input className="input" name="phone_num" type="text" placeholder="Enter phone number"></input>
                     </div>
                 </div>
                 <div className="field">
                     <label className="label">Message body</label>
                     <div className="control">
-                    <textarea class="textarea" placeholder="Enter message body"></textarea>
+                    <textarea className="textarea" name="message" placeholder="Enter message body"></textarea>
                     </div>
                 </div>
-                <button type="submit" class="button is-warning is-size-6">Submit</button>
+                <button type="submit" className="button is-warning is-size-6">Submit</button>
             </form>
         </div>
     )
